44c45122dedc3a3052f5904d9b847ea3dd69c305
Anjul <anjulmohammed@gmail.com>
2025-10-20 16:09:32 +0530

    changed regisatarytion

--- telelink/settings.py
+++ telelink/settings.py
@@ -84,20 +84,8 @@ DATABASES = {
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
 
-AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
-]
+# Remove all password validations
+AUTH_PASSWORD_VALIDATORS = []
 
 
 # Internationalization
